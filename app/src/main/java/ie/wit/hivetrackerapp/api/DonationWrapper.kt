9b3b1322e46cecaa4594d639f9ed60563041a4c8
Sheamus Clifford <faugh01@gmail.com>
2021-12-05 01:24:06 +0000

    refactor to use MVVM started adding api

---
+++ app/src/main/java/ie/wit/hivetrackerapp/api/DonationWrapper.kt
@@ -0,0 +1,8 @@
+package ie.wit.hivetrackerapp.api
+
+import ie.wit.donationx.models.DonationModel
+
+class DonationWrapper {
+    var message: String? = null
+    var data: DonationModel? = null
+}
---
+++ app/src/main/java/ie/wit/hivetrackerapp/api/HiveClient.kt
@@ -0,0 +1,30 @@
+package ie.wit.hivetrackerapp.api
+
+import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+object HiveClient {
+
+        val serviceURL = "https://desolate-lowlands-41702.herokuapp.com"
+
+        fun getApi() : HiveService {
+
+            val gson = GsonBuilder().create()
+
+            val okHttpClient = OkHttpClient.Builder()
+                .connectTimeout(30, TimeUnit.SECONDS)
+                .writeTimeout(30, TimeUnit.SECONDS)
+                .readTimeout(30, TimeUnit.SECONDS)
+                .build()
+
+            val apiInterface = Retrofit.Builder()
+                .baseUrl(serviceURL)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .client(okHttpClient)
+                .build()
+            return apiInterface.create(HiveService::class.java)
+        }
+    }
